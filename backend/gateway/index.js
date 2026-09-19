614ff8565962a2b118cceeb04a24de7dba5bede3
Rahul-g-7 <rahulg80730@gmail.com>
2026-09-19 22:57:08 +0530

    feat: initialize express gateway server with basic middleware

---
+++ backend/gateway/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+
+app.listen(PORT, () => {
+    console.log(`Gateway is running on port ${PORT}`);
+});
