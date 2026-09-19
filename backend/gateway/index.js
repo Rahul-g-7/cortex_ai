ee381c8fe243f2d8435d49bdc9086efcb26ce5fb
Rahul-g-7 <rahulg80730@gmail.com>
2026-09-19 23:14:44 +0530

    chore: add node_modules dependencies for auth service

--- backend/gateway/index.js
+++ backend/gateway/index.js
@@ -10,6 +10,9 @@ const PORT = process.env.PORT || 8000;
 app.use(express.json());
 app.use(morgan("dev"));
 
+app.get("/", (req, res) => {
+    res.json({ message: "hello from gateway service" })
+})
 
 app.listen(PORT, () => {
     console.log(`Gateway is running on port ${PORT}`);
