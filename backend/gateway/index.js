4e80741c7804c41c3caa939255c8454329ff9ad3
Rahul-g-7 <rahulg80730@gmail.com>
2026-09-19 23:43:18 +0530

    feat: initialize gateway service with express and auth proxy

--- backend/gateway/index.js
+++ backend/gateway/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
-
+import proxy from "express-http-proxy";
 dotenv.config();
 
 const app = express();
@@ -9,11 +9,12 @@ const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(morgan("dev"));
-app.use("/auth",proxy(process.env.AUTH_SERVICE));
+
 
 app.get("/", (req, res) => {
     res.json({ message: "hello from gateway service" })
 })
+app.use("/auth",proxy(process.env.AUTH_SERVICE));
 
 app.listen(PORT, () => {
     console.log(`Gateway is running on port ${PORT}`);
