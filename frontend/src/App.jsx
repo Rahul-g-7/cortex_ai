16731459a9ff487423c957f04f5165f24accf146
Rahul-g-7 <rahulg80730@gmail.com>
2026-09-20 13:42:19 +0530

    feat: initialize frontend application with Tailwind CSS, Axios, and Firebase Google authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import {auth, googleProvider } from '../utils/firebase';
 import { signInWithPopup } from 'firebase/auth';
+import api from '../utils/axios';
 
 const App = () => {
+  const hanldeLogin=async(token)=>{
+    try {
+     const {data}= await api.post("/auth/login",token)
+     console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
   const googleLogin=async()=>{
   const data= await signInWithPopup(auth,googleProvider)
+  const token=await data.user.getIdToken()
+  console.log(token)
+  await hanldeLogin(token)
   console.log(data) 
   }
   return (
