41dd794805511046de808640ee00fd7d8c87a544
Rahul-g-7 <rahulg80730@gmail.com>
2026-09-20 12:03:25 +0530

    feat: add Firebase configuration and Google sign-in UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import {auth, googleProvider } from '../utils/firebase';
+import { signInWithPopup } from 'firebase/auth';
 
 const App = () => {
+  const googleLogin=async()=>{
+  const data= await signInWithPopup(auth,googleProvider)
+  console.log(data) 
+  }
   return (
     <div className='bg-gray-900 flex items-center justify-center h-screen'>
-      <button className='bg-white text-blue-500 px-6 py-2.5 rounded-lg shadow-md flex items-center gap-2 border border-gray-300 cursor-pointer'>Continue with google</button>
+      <button onClick={googleLogin} className='bg-white text-blue-500 px-6 py-2.5 rounded-lg shadow-md flex items-center gap-2 border border-gray-300 cursor-pointer'>Continue with google</button>
     </div>
   );
 }
