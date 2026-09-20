14192e98fecb0c3f75eea61ebc316fe022320abe
Rahul-g-7 <rahulg80730@gmail.com>
2026-09-20 15:07:08 +0530

    feat: implement login and logout controllers with Firebase and Redis session management

--- backend/services/auth/controllers/auth.controller.js
+++ backend/services/auth/controllers/auth.controller.js
@@ -15,8 +15,14 @@ export const login = async(req, res) => {
                 avatar:decoded.picture
             })
         }
-        redis
+        
         const sessionID=crypto.randomUUID();
+        await redis.set(`session:${sessionID}`,JSON.stringify({
+            userId:user._id,
+            name:user.name,
+            email:user.email,
+            avatar:user.avatar
+        }),"EX",7*24*60*60)
         res.cookie("session",sessionID,{
             httpOnly:true,
             secure:false,
@@ -33,3 +39,17 @@ export const login = async(req, res) => {
     }
 }
 
+export const logout=async(req,res)=>{
+    try {
+        const sessionID=req.cookie?.session
+        if (!sessionID){
+            return res.status(400).json({message:"no session id"})
+        }
+        await redis.del(`session:${sessionID}`)
+        res.clearCookie("session")
+        return res.status(200).json({message:"logout successful"})
+    } catch (error) {
+        console.log("logout error ",error)
+        return res.status(500).json({message:"logout error"})
+    }
+}
