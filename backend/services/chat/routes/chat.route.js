b1b85c93d116b252e84e326002ee6194d7078a14
Rahul-g-7 <rahulg80730@gmail.com>
2026-09-24 00:35:59 +0530

    feat: update chat routes to include message handling and fix HTTP methods

--- backend/services/chat/routes/chat.route.js
+++ backend/services/chat/routes/chat.route.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { createConversation, getConversations } from "../controllers/chat.controller.js";
+import { createConversation, getConversations,saveMessage,updateConversation,getMessages } from "../controllers/chat.controller.js";
 const router = express.Router();
 
-router.post("/create-conversation", createConversation);
+router.get("/create-conversation", createConversation);
 router.get("/get-conversations", getConversations);
+router.post("/update-conversation", updateConversation);
 router.post("/save-message", saveMessage);
-router.get("/get-message", getMessage);
-router.put("/update-conversation", updateConversation);
+router.get("/get-messages/:conversationId", getMessages);
+
+export default router;
