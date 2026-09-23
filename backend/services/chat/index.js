3bb4673f7dc8f62af86634e4a6cf93441f86f24a
Rahul-g-7 <rahulg80730@gmail.com>
2026-09-23 23:03:46 +0530

    feat: initialize chat service with Express, MongoDB connection, and environment configuration

---
+++ backend/services/chat/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cookieParser())
+
+app.get("/",(req,res)=>{
+    res.send("hello from auth chat service")
+})
+
+app.listen(PORT, () => {
+    console.log(`chat is running on port ${PORT}`);
+    connectDB()
+});
