36882995aad6c261702d580521bfd19e1656b122
Rahul-g-7 <rahulg80730@gmail.com>
2026-09-24 23:14:34 +0530

    feat: enhance router logic with detailed agent descriptions and user query handling

--- backend/services/agent/graph/router.js
+++ backend/services/agent/graph/router.js
@@ -1,4 +1,67 @@
 import { getModel } from "../llm.js";
-export const router = async (params) => {
+export const router = async (state) => {
   const llm=await getModel("router")
+  const prompt=`You are an intelligent agent router for a multi-agent AI system.
+  
+  Available agents:
+  -chat
+  -search
+  -coding
+  -pdf
+  -ppt
+  -vision
+  
+  Rules:
+  chat:
+  General conversation,
+  explainations,
+  learning,
+  questions and answers.
+
+  search:
+  Current events,
+  latest news,
+  latest information,
+  recent updates,
+  recent developments,
+  internet lookup.
+
+  coding:
+  Generate code,
+  debug code,
+  build projects,
+  architecture,
+  API dessign.
+
+  pdf:
+  Questions about generate PDFs
+  or document context.
+
+  ppt:
+  Questions about generate PPTs
+  or ppt context.
+
+  vision:
+  generate image,
+  create image,
+
+
+  Return ONLY one word:
+
+  chat 
+  search
+  coding
+  pdf
+  ppt
+  vision
+
+  User Query:
+  ${state.prompt}
+  `
+  const response=await llm.call(prompt)
+  return {
+    ...state,
+    agent:response
+  }
+  
 }
