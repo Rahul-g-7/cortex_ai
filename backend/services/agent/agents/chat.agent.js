3a2a4069a79f2bfb2c6145e47a981e23591b7dc1
Rahul-g-7 <rahulg80730@gmail.com>
2026-09-24 23:42:17 +0530

    feat: implement chat agent logic with LLM integration and response handling

--- backend/services/agent/agents/chat.agent.js
+++ backend/services/agent/agents/chat.agent.js
@@ -1,3 +1,20 @@
-export const chatAgent=async (params) => {
-
-}
+import { getModel } from "../../llm/llm.js";
+export const chatAgent = async (state) => {
+  const llm = await getModel("chat");
+  const prompt = "Your are a chat Assitant";
+  const response = await llm.invoke(
+    {
+      role: "sysem",
+      content: prompt,
+    },
+    {
+      role: "user",
+      content: state.prompt,
+    },
+  );
+  return {
+    ...state,
+    aiResponse: response.content,
+    agent: "chat",
+  };
+};
