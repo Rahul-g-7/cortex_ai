392fd4c521ff55f604b724dec052e5cf3b089081
Rahul-g-7 <rahulg80730@gmail.com>
2026-09-26 17:54:13 +0530

    bug fix

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,6 +1,6 @@
 import {configureStore} from "@reduxjs/toolkit";
 import userReducer from './userSlice';
-import conversationReducer from "   ./conversationSlice"
+import conversationReducer from "./conversationSlice"
 export const store=configureStore({
     reducer:{
         user: userReducer,
